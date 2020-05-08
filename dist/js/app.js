d7fea2ed7c6e3057375c5ecc5ef851f2ad309476
pep1994 <marcopetrini1994@live.it>
2020-05-08 17:23:52 +0200

    code refactoring e modifiche di stile

--- dist/js/app.js
+++ dist/js/app.js
@@ -95,6 +95,26 @@
 
 $(document).ready(init);
 
+function print(array) {
+  var source = $("#album-template").html();
+  var template = Handlebars.compile(source);
+  array.forEach(function (element) {
+    console.log(element);
+    var context = {
+      title: element.title,
+      author: element.author,
+      year: element.year,
+      album: element.poster,
+      genere: element.genre
+    };
+    $('.container').append(template(context));
+  });
+}
+
+function filter() {
+  $('#search').change(function () {});
+}
+
 function init() {
   $.ajax({
     method: "GET",
@@ -102,34 +122,8 @@ function init() {
     dataType: "json",
     success: function success(data) {
       var arrayResult = data;
-      var source = $("#album-template").html();
-      var template = Handlebars.compile(source);
-      arrayResult.forEach(function (element) {
-        console.log(element);
-        var context = {
-          title: element.title,
-          author: element.author,
-          year: element.year,
-          album: element.poster,
-          genere: element.genre
-        };
-        $('.container').append(template(context));
-      });
-      $('select').change(function () {
-        var valSelect = $('select').val();
-        $('.cover').hide();
-        $('.cover').each(function () {
-          var genreCover = $(this).data('genere').toLowerCase();
-
-          if (genreCover == valSelect) {
-            $(this).show();
-          }
-        });
-
-        if (valSelect == 'all') {
-          $('.cover').show();
-        }
-      });
+      print(arrayResult);
+      filter();
     },
     error: function error(_error) {
       alert(_error);
